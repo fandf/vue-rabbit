a089ba5fa6d8d3e46350e765f016566c174fe84b
fdf <365283959@qq.com>
2023-08-05 14:36:44 +0800

    新鲜好物

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,19 @@
 import httpInstance from "@/utils/http";
 
+/**
+ * 获取banner
+ */
 export function getBannerAPI() {
   return httpInstance({
     url: "home/banner",
+  });
+}
+
+/**
+ * 获取新鲜好物
+ */
+export function findNewAPI() {
+  return httpInstance({
+    url: "home/new",
   });
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,15 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" sub-title="好多商品啊">
+    <div>
+      我是新鲜好污的插槽
+    </div>
+  </HomePanel>
+  <HomePanel title="人机推荐" sub-title="好多推荐啊">
+    <div>
+      我是人机推荐的插槽
+    </div>
+  </HomePanel>
 </template>
