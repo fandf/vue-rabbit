d72420dca89412a32b1e645bc6a76f4a7e19c0b5
fdf <365283959@qq.com>
2023-08-05 12:57:49 +0800

    整体结构拆分和分类实现

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
 
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+// 引入初始化样式文件
+import "@/styles/common.scss";
 
-import App from './App.vue'
-import router from './router'
+// 引入懒加载指令插件并且注册
+// import { lazyPlugin } from "@/directives";
+// 引入全局组件插件
+// import { componentPlugin } from "@/components";
 
-import '@/styles/common.scss'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+// app.use(lazyPlugin);
+// app.use(componentPlugin);
+app.mount("#app");
