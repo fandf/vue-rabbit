83985e1e54cf8d76b4d37222b689000f4fd69692
fdf <365283959@qq.com>
2023-08-06 17:29:36 +0800

    分页无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {getCategoryFilterAPI} from '@/apis/category'
+import {getCategoryFilterAPI, getSubCategoryAPI} from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import  GoodsItem  from '../Home/components/GoodsItem.vue'
 
 // 获取面包屑导航数据
 const route = useRoute()
@@ -14,6 +15,40 @@ const getCategoryData = async ()=>{
 
 onMounted(()=> getCategoryData())
 
+// 获取基础列表数据
+
+const goodList = ref([])
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+const getGoodList = async ()=>{
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = res.result.items
+}
+
+onMounted(()=> getGoodList())
+
+
+// tab切换
+const tabChange = ()=>{
+  console.log('tab切换了', reqData.value.sortField)
+  getGoodList()
+}
+
+// 无限加载
+const disabled = ref(false)
+const load = async ()=>{
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕，停止监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -28,13 +63,14 @@ onMounted(()=> getCategoryData())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
+         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
     </div>
   </div>
