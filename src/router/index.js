bdc317df42e2a3dd9578ddf17b9259cb53d6739e
fdf <365283959@qq.com>
2023-08-06 10:56:44 +0800

    分类路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            component: Layout,
-            children: [
-                {path: '', component: Home},
-                {path: 'category', component: Category},
-            ]
-        },
-        {
-            path: '/login',
-            component: Login
-        }
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: Layout,
+      children: [
+        { path: "", component: Home },
+        { path: "category/:id", component: Category },
+      ],
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
