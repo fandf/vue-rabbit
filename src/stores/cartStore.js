99eea6e1814b4efa74102c59b6c40bbc07c7cd4f
fdf <365283959@qq.com>
2023-08-13 17:12:39 +0800

    本地购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 export const useCartStore = defineStore(
   "cart",
@@ -13,9 +13,58 @@ export const useCartStore = defineStore(
         cartList.value.push(goods);
       }
     };
+
+    const delCart = (skuId) => {
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId);
+      cartList.value.splice(idx, 1);
+    };
+
+    const allCount = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count, 0)
+    );
+
+    const allPrice = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count * c.price, 0)
+    );
+
+    // 购物车单选功能
+    const singleCheck = (skuId, selected) => {
+      const item = cartList.value.find((item) => item.skuId === skuId);
+      item.selected = selected;
+    };
+
+    // 是否全选
+    const isAll = computed(() => cartList.value.every((item) => item.selected));
+
+    // 全选
+    const allCheck = (selected) => {
+      cartList.value.forEach((item) => (item.selected = selected));
+    };
+
+    // 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count, 0)
+    );
+    // 已选择价格
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count * c.price, 0)
+    );
+
     return {
       cartList,
+      allCount,
+      allPrice,
+      selectedCount,
+      selectedPrice,
       addCart,
+      delCart,
+      singleCheck,
+      isAll,
+      allCheck,
     };
   },
   {
